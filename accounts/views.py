d1811db0c3817cefeb1c78753471141114eb9d62
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-06-03 14:51:33 +0300

    Unanswered tickets added to homepage with Modify button

--- accounts/views.py
+++ accounts/views.py
@@ -24,12 +24,17 @@ def homepage(request):
     followed_users = [relation.followed_user for relation in UserFollows.objects.filter(user=request.user)]
     followed_tickets_list = [ticket for ticket in Ticket.objects.filter(user__in=followed_users)]
     reviews_tickets_list = [review.ticket for review in Review.objects.all()]
+
     answered_tickets_list = [ticket for ticket in followed_tickets_list if ticket in reviews_tickets_list]
     followed_answered_tickets = Ticket.objects.filter(title__in=answered_tickets_list)
-    unanswered_tickets_list = [ticket for ticket in followed_tickets_list if ticket not in reviews_tickets_list]
-    followed_unanswered_tickets = Review.objects.filter(ticket__in=unanswered_tickets_list)
-    unanswered_tickets = followed_unanswered_tickets.annotate(content_type=Value('UNANSWERED_TICKET', CharField()))
     answered_tickets = followed_answered_tickets.annotate(content_type=Value('ANSWERED_TICKET', CharField()))
+
+    my_unanswered_tickets = [ticket for ticket in user_tickets if ticket not in reviews_tickets_list]
+    unanswered_followed_tickets = [ticket for ticket in followed_tickets_list if ticket not in reviews_tickets_list]
+    unanswered_followed_tickets.extend(my_unanswered_tickets)
+    followed_unanswered_tickets = Ticket.objects.filter(title__in=unanswered_followed_tickets)
+    unanswered_tickets = followed_unanswered_tickets.annotate(content_type=Value('UNANSWERED_TICKET', CharField()))
+
     followed_users_reviews = Review.objects.filter(user__in=followed_users)
     followed_reviews = followed_users_reviews.annotate(content_type=Value('FOLLOWED_REVIEW', CharField()))
     posts = sorted(
